ca64b8dcde1bdc2c3b4a4521a8915e9f402a8fb9
Sabab Asmae <sababasmae@gmail.com>
2024-09-09 18:56:18 +0200

    Added the files for the remaining tasks 2-4

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,6 @@
+
+print("How old are you?")
+age = int(input())
+
+
+print("In 2050, you will be {} years old.".format(age + 27))
---
+++ python_introduction/rectangle_area.py
@@ -0,0 +1,5 @@
+
+length = 10
+width = 5
+
+print("The area of the rectangle  is: {}".format(length * width))
