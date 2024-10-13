bc63f44c615f0328d56fab762a6ab4c14515821f
Sabab Asmae <sababasmae@gmail.com>
2024-10-13 21:23:05 +0100

    Fix library system printing strings

--- oop/library_system.py
+++ oop/library_system.py
@@ -1,10 +1,13 @@
 class Book:
-    def __init__(self, title: str, author: str):
+    def __init__(self, title, author):
         self.title = title
         self.author = author
 
+    def __str__(self):
+        return f"Book: {self.title} by {self.author}"
+
 class EBook(Book):
-    def __init__(self, title: str, author: str, file_size: int):
+    def __init__(self, title, author, file_size):
         super().__init__(title, author)
         self.file_size = file_size
 
@@ -12,7 +15,7 @@ class EBook(Book):
         return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
 
 class PrintBook(Book):
-    def __init__(self, title: str, author: str, page_count: int):
+    def __init__(self, title, author, page_count):
         super().__init__(title, author)
         self.page_count = page_count
 
@@ -28,4 +31,4 @@ class Library:
 
     def list_books(self):
         for book in self.books:
-            print(book)
+            print(str(book))  
